a5dd5e484ef50ab133d55e818f767d63264a68ce
Mary Selig <seligmar@gmail.com>
2019-02-23 12:46:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
+<<<<<<< HEAD
   kittens.push(name); 
   return kittens; 
 }
@@ -41,3 +42,9 @@ function removeFirstKitten() {
   var x = kittens.slice(1); 
   return x;  
 }
+=======
+  var x = kittens.push(name); 
+  return x 
+}
+
+>>>>>>> 0072a03b858202bea9889a2d07def9f124eec745
